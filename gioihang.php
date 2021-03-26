3c4a4d7a9297ed92544fbffd8435b040c1d6e48c
Nguyen Hung Manh <manh.nguyen22@pn.com.vn>
2021-03-26 19:36:11 +0700

    first commit

---
+++ gioihang.php
@@ -0,0 +1,297 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <title>
+    </title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/ttt.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+    <div class="page-index">
+    </div>
+    </div>
+    <div class="clearfix">
+    </div>
+    <div class="container_fullwidth">
+        <div class="container shopping-cart">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title">
+                        Shopping Cart
+                    </h3>
+                    <p>
+                    </p>
+                    <div class="clearfix">
+                    </div>
+                    <table class="shop-table">
+                        <thead>
+                            <tr>
+                                <th>Image</th>
+                                <th>Dtails</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                                <th> Delete </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="images/products/small/products-06.png" alt="">
+                                </td>
+                                <td>
+                                    <div class="shop-details">
+                                        <div class="productname">
+                                            Lincoln Corner Unit Products
+                                        </div>
+                                        <p>
+                                            <img alt="" src="images/star.png">
+                                            <a class="review_num" href="#">
+                            02 Review(s)
+                          </a>
+                                        </p>
+                                        <div class="color-choser">
+                                            <span class="text">
+                            Product Color : 
+                          </span>
+                                            <ul>
+                                                <li>
+                                                    <a class="black-bg " href="#">
+                                black
+                              </a>
+                                                </li>
+                                                <li>
+                                                    <a class="red-bg" href="#">
+                                light red
+                              </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <p>
+                                            Product Code :
+                                            <strong class="pcode">
+                            Dress 120
+                          </strong>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <h5>
+                                        $200.00
+                                    </h5>
+                                </td>
+                                <td>
+                                    <select name="">
+                        <option selected value="1">
+                          1
+                        </option>
+                        <option value="1">
+                          2
+                        </option>
+                        <option value="1">
+                          3
+                        </option>
+                      </select>
+                                </td>
+                                <td>
+                                    <h5>
+                                        <strong class="red">
+                          $200.00
+                        </strong>
+                                    </h5>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <img src="images/remove.png" alt="">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/products/small/products-02.png" alt="">
+                                </td>
+                                <td>
+                                    <div class="shop-details">
+                                        <div class="productname">
+                                            Lincoln Corner Unit Products
+                                        </div>
+                                        <p>
+                                            <img alt="" src="images/star.png">
+                                            <a class="review_num" href="#">
+                            02 Review(s)
+                          </a>
+                                        </p>
+                                        <div class="color-choser">
+                                            <span class="text">
+                            Product Color : 
+                          </span>
+                                            <ul>
+                                                <li>
+                                                    <a class="gray-bg" href="#">
+                                pink
+                              </a>
+                                                </li>
+                                                <li>
+                                                    <a class="black-bg " href="#">
+                                black
+                              </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <p>
+                                            Product Code :
+                                            <strong class="pcode">
+                            Dress 132
+                          </strong>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <h5>
+                                        $200.00
+                                    </h5>
+                                </td>
+                                <td>
+                                    <select name="">
+                        <option selected value="1">
+                          1
+                        </option>
+                        <option value="2">
+                          2
+                        </option>
+                        <option value="3">
+                          3
+                        </option>
+                      </select>
+                                </td>
+                                <td>
+                                    <h5>
+                                        <strong class="red">
+                          $200.00
+                        </strong>
+                                    </h5>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <img src="images/remove.png" alt="">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/products/small/products-05.png" alt="">
+                                </td>
+                                <td>
+                                    <div class="shop-details">
+                                        <div class="productname">
+                                            Lincoln Corner Unit Products
+                                        </div>
+                                        <p>
+                                            <img alt="" src="images/star.png">
+                                            <a class="review_num" href="#">
+                            02 Review(s)
+                          </a>
+                                        </p>
+                                        <div class="color-choser">
+                                            <span class="text">
+                            Product Color : 
+                          </span>
+                                            <ul>
+                                                <li>
+                                                    <a class="red-bg" href="#">
+                                light red
+                              </a>
+                                                </li>
+                                                <li>
+                                                    <a class=" yellow-bg" href="#">
+                                yellow"
+                              </a>
+                                                </li>
+                                                <li>
+                                                    <a class="black-bg " href="#">
+                                black
+                              </a>
+                                                </li>
+                                                <li>
+                                                    <a class="pink-bg" href="#">
+                                pink
+                              </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <p>
+                                            Product Code :
+                                            <strong class="pcode">
+                            Dress 050
+                          </strong>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <h5>
+                                        $200.00
+                                    </h5>
+                                </td>
+                                <td>
+                                    <select name="">
+                        <option selected value="1">
+                          1
+                        </option>
+                        <option value="2">
+                          2
+                        </option>
+                        <option value="3">
+                          3
+                        </option>
+                      </select>
+                                </td>
+                                <td>
+                                    <h5>
+                                        <strong class="red">
+                          $200.00
+                        </strong>
+                                    </h5>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <img src="images/remove.png" alt="">
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="6">
+                                    <button class="pull-left">
+                        Continue Shopping
+                      </button>
+                                    <button class=" pull-right">
+                        Update Shopping Cart
+                      </button>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                    <div class="clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Bootstrap core JavaScript==================================================-->
+
+    <script type="text/javascript" src="js/bootstrap.min.js">
+    </script>
+</body>
+
+</html>
